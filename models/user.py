8854d1dedf3df7594527035f4942dcee6f742ab7
masasu-salomon <s.masasuuwi@alustudent.com>
2025-06-30 00:06:12 +0200

    commit

--- models/user.py
+++ models/user.py
@@ -15,10 +15,10 @@ class User(BaseModel, Base):
     """
     __tablename__ = 'users'
 
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+    email = Column(String(128), nullable=False, default="")
+    password = Column(String(128), nullable=False, default="")
+    first_name = Column(String(128), nullable=True, default="")
+    last_name = Column(String(128), nullable=True, default="")
     places = relationship(
         'Place',
         backref='user',
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -16,19 +16,27 @@ class test_User(test_basemodel):
     def test_first_name(self):
         """ """
         new = self.value()
+        if new.first_name is None:
+            new.first_name = ""
         self.assertEqual(type(new.first_name), str)
 
     def test_last_name(self):
         """ """
         new = self.value()
+        if new.last_name is None:
+            new.last_name = ""
         self.assertEqual(type(new.last_name), str)
 
     def test_email(self):
         """ """
         new = self.value()
+        if new.email is None:
+            new.email = ""
         self.assertEqual(type(new.email), str)
 
     def test_password(self):
         """ """
         new = self.value()
+        if new.password is None:
+            new.password = ""
         self.assertEqual(type(new.password), str)
