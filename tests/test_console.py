4e327118b4bf98907934b1db669534a56fecc307
masasu-salomon <s.masasuuwi@alustudent.com>
2025-06-29 23:48:11 +0200

    commit

---
+++ tests/test_console.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Unit tests for the console """
+import unittest
+from console import HBNBCommand
+import sys
+from io import StringIO
+
+class TestConsole(unittest.TestCase):
+    """Test the console commands"""
+    def setUp(self):
+        self.console = HBNBCommand()
+        self.output = StringIO()
+        self._stdout = sys.stdout
+        sys.stdout = self.output
+
+    def tearDown(self):
+        sys.stdout = self._stdout
+
+    def test_quit_command(self):
+        """Test quit command exits the console"""
+        with self.assertRaises(SystemExit):
+            self.console.onecmd("quit")
+
+    def test_EOF_command(self):
+        """Test EOF command exits the console"""
+        with self.assertRaises(SystemExit):
+            self.console.onecmd("EOF")
+
+    def test_emptyline(self):
+        """Test empty line does nothing"""
+        self.console.onecmd("")
+        self.assertEqual(self.output.getvalue(), "")
+
+if __name__ == "__main__":
+    unittest.main()
