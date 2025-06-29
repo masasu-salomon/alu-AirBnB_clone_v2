4d78e09c018259029dfdf9db74bb5286d24bacd0
masasu-salomon <s.masasuuwi@alustudent.com>
2025-06-29 20:25:30 +0200

    commit

--- models/base_model.py
+++ models/base_model.py
@@ -2,10 +2,10 @@
 """This module defines a base class for all models in our hbnb clone"""
 
 from sqlalchemy.ext.declarative import declarative_base
-Base = declarative_base()
-
 import uuid
 from datetime import datetime
+
+Base = declarative_base()
 
 
 class BaseModel:
