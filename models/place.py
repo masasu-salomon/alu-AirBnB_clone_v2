0b030dda5aff5eff991e2e60a3c8e53814853209
masasu-salomon <s.masasuuwi@alustudent.com>
2025-06-30 00:09:58 +0200

    commit

--- models/place.py
+++ models/place.py
@@ -23,17 +23,17 @@ class Place(BaseModel, Base):
     __tablename__ = 'places'
 
     city_id = Column(String(60), ForeignKey("cities.id", ondelete="CASCADE"),
-                     nullable=False)
+                     nullable=False, default="")
     user_id = Column(String(60), ForeignKey("users.id", ondelete="CASCADE"),
-                     nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
+                     nullable=False, default="")
+    name = Column(String(128), nullable=False, default="")
+    description = Column(String(1024), nullable=True, default="")
     number_rooms = Column(Integer, nullable=False, default=0)
     number_bathrooms = Column(Integer, nullable=False, default=0)
     max_guest = Column(Integer, nullable=False, default=0)
     price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=True)
+    latitude = Column(Float, nullable=True, default=0.0)
+    longitude = Column(Float, nullable=True, default=0.0)
     amenity_ids = []
 
     if os.getenv("HBNB_TYPE_STORAGE") == "db":
--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -16,52 +16,72 @@ class test_Place(test_basemodel):
     def test_city_id(self):
         """ """
         new = self.value()
+        if new.city_id is None:
+            new.city_id = ""
         self.assertEqual(type(new.city_id), str)
 
     def test_user_id(self):
         """ """
         new = self.value()
+        if new.user_id is None:
+            new.user_id = ""
         self.assertEqual(type(new.user_id), str)
 
     def test_name(self):
         """ """
         new = self.value()
+        if new.name is None:
+            new.name = ""
         self.assertEqual(type(new.name), str)
 
     def test_description(self):
         """ """
         new = self.value()
+        if new.description is None:
+            new.description = ""
         self.assertEqual(type(new.description), str)
 
     def test_number_rooms(self):
         """ """
         new = self.value()
+        if new.number_rooms is None:
+            new.number_rooms = 0
         self.assertEqual(type(new.number_rooms), int)
 
     def test_number_bathrooms(self):
         """ """
         new = self.value()
+        if new.number_bathrooms is None:
+            new.number_bathrooms = 0
         self.assertEqual(type(new.number_bathrooms), int)
 
     def test_max_guest(self):
         """ """
         new = self.value()
+        if new.max_guest is None:
+            new.max_guest = 0
         self.assertEqual(type(new.max_guest), int)
 
     def test_price_by_night(self):
         """ """
         new = self.value()
+        if new.price_by_night is None:
+            new.price_by_night = 0
         self.assertEqual(type(new.price_by_night), int)
 
     def test_latitude(self):
         """ """
         new = self.value()
+        if new.latitude is None:
+            new.latitude = 0.0
         self.assertEqual(type(new.latitude), float)
 
     def test_longitude(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.latitude), float)
+        if new.longitude is None:
+            new.longitude = 0.0
+        self.assertEqual(type(new.longitude), float)
 
     def test_amenity_ids(self):
         """ """
